455ae11f086de8d84675936e8f35169818593a26
Todd Radel <todd@hashicorp.com>
2019-04-17 11:24:34 -0400

    Let the app start and create database objects

--- build/broadleaf.sh
+++ build/broadleaf.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -e
 
-# Install JRE and Maven
-apt-get -y install default-jdk maven
-
 # Clone the Broadleaf Commerce code from GitHub
 if [ ! -d /opt/DemoSite ]; then
   git clone https://github.com/tradel/DemoSite.git /opt/DemoSite
@@ -15,3 +12,11 @@ mvn install
 # Install unit files for systemd
 install -c -m 0644 /tmp/systemd/broadleaf-site.service /etc/systemd/system
 install -c -m 0644 /tmp/systemd/broadleaf-admin.service /etc/systemd/system
+
+# Start the application and populate the MySQL database
+cd /opt/DemoSite/site 
+mvn spring-boot:start 
+
+# Stop the app. We should be able to do "mvn spring-boot:stop" here, but it
+# complains and I haven't figured it out.
+pkill java 
