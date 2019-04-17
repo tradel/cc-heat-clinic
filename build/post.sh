59f0cd30ab7789b6779527f8c8770fed6ea0609b
Todd Radel <todd@hashicorp.com>
2019-04-17 11:22:43 -0400

    Clean up Solr and MySQL before shutdown

--- build/post.sh
+++ build/post.sh
@@ -16,3 +16,14 @@ install -d -m 0700 -o demo -g demo /home/demo/.ssh
 install -c -m 0600 -o demo -g demo /tmp/ssh/id_ecdsa.pub /home/demo/.ssh/authorized_keys
 install -c -m 0600 -o demo -g demo /tmp/ssh/id_ecdsa /home/demo/.ssh/id_ecdsa
 install -c -m 0644 -o demo -g demo /tmp/ssh/id_ecdsa.pub /home/demo/.ssh/id_ecdsa.pub
+
+# Stop MySQL before we shut down the box
+systemctl stop mysql 
+systemctl disable mysql 
+
+# Stop Solr index server
+systemctl stop solr 
+systemctl disable solr 
+
+# Clean out cruft in /tmp
+rm -rf /tmp/*
